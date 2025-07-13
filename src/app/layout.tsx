d21b764e34433642506f2826e2a71d8ecbba50c8
raihara3 <raihara3@gmail.com>
2025-07-14 02:08:58 +0900

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SpaceBackground from "@/components/SpaceBackground";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
       >
         <SpaceBackground />
         {children}
+        <Analytics />
       </body>
     </html>
   );
